0827cec5eed6ce41b8f3633e78ecad1faacb881a
지은성 <ddrr2852@gmail.com>
2025-12-24 14:48:22 +0900

    feat : appbar 및 empty 생성 및 연결

---
+++ lib/empty_page/empty_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmptyPage extends StatelessWidget {
+  const EmptyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("아무것도 없는 페이지"));
+  }
+
+  //
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mini_memo/empty_page/empty_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [
-        
-      ],
-    ));
+    return Scaffold(
+      //appBar 은성
+      appBar: AppBar(
+        title: Text(
+          '동 주니어',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(children: [EmptyPage()]),
+    );
   }
 }
