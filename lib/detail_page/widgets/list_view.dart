0eb3c9889c7f916a9dccd827f286176d46220351
kiwikid1543-alt <kiwikid1543@gmail.com>
2025-12-24 20:10:11 +0900

    feat: list 수정

--- lib/detail_page/widgets/list_view.dart
+++ lib/detail_page/widgets/list_view.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:mini_memo/detail_page/detail_page.dart';
 import 'package:mini_memo/detail_page/widgets/list_item.dart';
+import 'package:mini_memo/memo_entity/memo_entity.dart';
 
 class NormalList extends StatelessWidget {
-  const NormalList({super.key});
+  const NormalList({super.key, required this.memoList});
+
+  final List<MemoEntity> memoList;
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
       padding: const EdgeInsets.all(16),
-      itemCount: 10,
+      itemCount: memoList.length,
       separatorBuilder: (_, _) => const SizedBox(height: 12),
       itemBuilder: (context, index) {
-        return ListItem();
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DetailPage()),
+            );
+          },
+          child: ListItem(memoList: memoList, index: index),
+        );
       },
     );
   }
